e4f5901683e0f645af801a3fd5de7a11cd58292b
tech <tech@kali.kali>
2023-02-20 16:23:30 +0200

    Feat():CRUD OPERATIONS -GET all users -GET single user by id -CREATE user [#Finishes --002]

--- src/api.js
+++ src/api.js
@@ -19,6 +19,36 @@ app.get("/users", (req, res) => {
   });
 });
 
+// get single user by id
+app.get("/user/:id", (req, res) => {
+  client.query(
+    `select * from users where id=${req.params.id} `,
+    (err, result) => {
+      if (!err) {
+        // console.log("here is result", result.rows);
+        res.send({ status: "success", data: result.rows });
+      } else {
+        res.send({ status: "fail", error: err.message });
+      }
+    }
+  );
+});
+
+// insert new user
+app.post("/user/post", (req, res) => {
+  const user = req.body;
+
+  const insertQuery = `INSERT INTO users (id, "firstName", "lastName", "location") VALUES (${user.id}, '${user.firstName}', '${user.lastName}', '${user.location}')`;
+
+  client.query(insertQuery, (err, result) => {
+    if (!err) {
+      res.send({ status: "success", message: "user created successfully" });
+    } else {
+      res.send({ status: "fail", error: err.message });
+    }
+  });
+});
+
 connect();
 
 app.listen(3000, () => {
